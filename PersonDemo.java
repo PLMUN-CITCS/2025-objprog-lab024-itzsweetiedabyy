929be7a6991853ab2e1de590ee28b313345a9ea5
itzsweetiedabyy <llanezadeniseannebernadette_bscs@plmun.edu.ph>
2025-03-08 22:08:00 +0800

    Submitting OBJPROG Week 05 - Laboratory # 24

---
+++ PersonDemo.java
@@ -0,0 +1,26 @@
+public class PersonDemo {
+    public static void main(String[] args) {
+       
+       class Person {
+        String name;
+        int age;
+        
+        public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+        public Person() {
+        this.name = "Unknown";
+        this.age = 0;
+    }
+    public void displayPerson() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
+        Person person1 = new Person("Bob", 30);
+        Person person2 = new Person(); 
+        
+        person1.displayPerson();
+        person2.displayPerson();
+    }
+}
